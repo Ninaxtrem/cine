c130fced9c9b2c31365104f5ad5947dac50df823
Ninaxtrem <nina@starxtrem.com>
2022-02-28 15:49:31 +0100

    liens

---
+++ cine.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/cine.css">
+    <title>cinema</title>
+</head>
+<body>
+    
+
+
+
+
+
+<script src="assets/js/cine.js"></script>
+</body>
+</html>
