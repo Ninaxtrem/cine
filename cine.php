cf5314f3002a342bb9eb9aba0a1395fece52d968
Ninaxtrem <nina@starxtrem.com>
2022-03-03 15:55:33 +0100

    debut footer

--- cine.php
+++ cine.php
@@ -4,11 +4,17 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Orbitron:wght@600&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="assets/css/cine.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Gloria+Hallelujah&display=swap" rel="stylesheet">
     <title>cinema</title>
 </head>
 <body>
-
+<!-- navbar -->
 <nav>
 
   <div class="bouton">
@@ -25,17 +31,59 @@
    </p>
   </div>
 </nav>
+<!-- fin nav-->
 
+<!-- bg -->
 <div class="img">
- <img src="assets/img/6525.jpg">
+ <img src="assets/img/Netflix_bg-c198d08298866bfd0ad539ce04a0dd9468de1c170cc56a91855c596cec25d620.jpg">
   </div>
+<!-- fin bg-->
 
+<!-- chez vous -->
   <h2>Le cinéma chez vous</h2>
 
-  
+  <div class="contenaire">
+    <div class="box1">
+    </div>
+    <div class="box2">
+      <div class="metro">
+        <p>
+       Vous voulez voir, un bon film, mais pas sortir de chez vous ? 
+       J'ai la solution Métropolis pour 5,99€ par moi. Avec toutes les nouveautés mais avec le confort à la maison.
+        </p>
+      </div>
+    </div>
+  </div>
 
+<!-- fin chez vous-->
 
+<!--paralax -->
+<div class="para"></div>
+<!-- fin paralax -->
 
+<!--metropolis -->
+  <div class="contenaire2">
+    <div class="box3">
+    <p>
+       Vous voulez voir, un bon film, mais pas sortir de chez vous ? 
+       J'ai la solution Métropolis pour 5,99€ par moi. Avec toutes les nouveautés mais avec le confort à la maison.
+        </p>
+    </div>
+    <div class="box4">
+      </div>
+    </div>
+  </div>
+ <!--fin metropolis -->
+
+ <!--footer -->
+  <div class="footer">
+  <div class="icone1"></div>
+  <div class="icone2"></div>
+  <div class="icone3"></div>
+  <div class="icone4"></div>
+  </div>
+ 
+ <!-- fin footer -->
 
 <script src="assets/js/cine.js"></script>
 </body>
