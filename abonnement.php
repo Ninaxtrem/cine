f3816800746140b635b428fd0cbba1d60fedc790
Ninaxtrem <nina@starxtrem.com>
2022-03-03 20:17:41 +0100

    abonnement

---
+++ abonnement.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Orbitron:wght@600&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/cine.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Gloria+Hallelujah&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <title>cinema</title>
+</head>
+<body>
+<!-- navbar -->
+<nav>
+
+  <div class="bouton">
+    <p>
+     <a href="#">Inscritpion</a>
+   </p>
+  </div>
+
+  <img src="assets/img/METROPOLIS_LOGO_ROND.png">
+  
+  <div class="bouton">
+    <p>
+     <a href="#">Se Connecter</a>
+   </p>
+  </div>
+</nav>
